ea0c968d7317a0cc1870174efb5aa51140b7cc5a
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-03 00:23:12 +0000

    using operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+
+"""
+operators in python : 
++ : addiction 
+- : soustraction
+* : multiplication 
+/ : devision
+% : modulo ( rest of a devision )
+
+"""
+
+calcul = 5 / 2
+calcul = int(calcul)
+print("the result is ", calcul)
