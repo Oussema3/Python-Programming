e1ee99f554c2d6d05ebf7cdf071142fc183730bf
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-03 00:24:33 +0000

    USING operators

---
+++ operators2.py
@@ -0,0 +1,15 @@
+levelOfCharacter =input("what is the level of your character? ")
+levelOfCharacter = int(levelOfCharacter)
+
+"""
+for incrementation we can use 
+variable = variable + 1
+or 
+variable += 1
+this is valid for all operations 
+"""
+
+
+print("Congrats ! u ve earned a level")
+levelOfCharacter += 1
+print("ur level now is:",levelOfCharacter)
