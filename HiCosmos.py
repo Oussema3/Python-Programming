195f3dff1e2834528052ef134d36ddf0eeb4e779
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-03 00:16:30 +0000

    Nada de Nade

---
+++ HiCosmos.py
@@ -0,0 +1,19 @@
+#un commentaire 
+
+""" 
+a cooment with 
+mulitiple lignes
+abnd this is the third ligne
+
+"""
+print("hello cosomos")
+
+print("another phrase")
+
+print('l\'envers du univer')
+
+print("i sometimes dnt hate my life ")
+
+print("a divided \nphrase")
+
+print ("\tthis phraase begins with tabululation")
