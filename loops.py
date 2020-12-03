3604b8566fa1c4da7f39deb7c05c3c5cb4e2c7f8
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-03 19:09:26 +0000

    While loop

---
+++ loops.py
@@ -0,0 +1,17 @@
+game_launch = True 
+while game_launch:
+    choise_menu = input("> ")
+    if choise_menu =="again":
+        continue
+    elif choise_menu =="quit":
+        break
+    elif choise_menu =="hello":
+        print("Good morning :) !")
+    else:
+        print("infound command :/ ")
+"""
+ i think that with enough data and conditions 
+ we can create a conversation with computer 
+ and the user
+"""
+print("Good Bye....")
