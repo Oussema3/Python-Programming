071b86c0ade667fa346197f651080d53922d6663
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-06 17:09:09 +0000

    modularity2

--- modularity2.py
+++ modularity2.py
@@ -10,3 +10,15 @@ def good_bye():
 
 
 # we will import this file in the modularity file
+"""
+we can add a test phase to see if this file is working good 
+or not
+this is done by this condition
+
+"""
+
+if __name__ == "__main__":
+    print("PHASE DE TEST DE modularity2.py")
+
+    parler("Jason ", "Good morning cosmos")
+    au_revoir()
