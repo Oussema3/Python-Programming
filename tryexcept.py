fcfc5ebda9e740cd5c93148af8f77f823e8005c0
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-06 17:58:39 +0000

    get age

---
+++ tryexcept.py
@@ -0,0 +1,12 @@
+age = input("what is ur age ")
+
+try:
+    age = int(age)
+    print("You have ",age,"years old")
+except:
+    print("enter a valid age")
+finally:
+    print("Program done !")
+
+
+    
