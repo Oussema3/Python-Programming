54143eefabd116c12f34a40c3f19da7a2bb878ca
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-06 18:06:07 +0000

    devision

--- tryexcept2.py
+++ tryexcept2.py
@@ -9,5 +9,7 @@ except ZeroDivisionError:
     print("You can devide by zero")
 except ValueError:
     print("enter a valid number")
+else:
+    print("Well done u have entered a valid value")
 finally:
     print("program is done")
