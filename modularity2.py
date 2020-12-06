6cc6ec9ce8423f24d5edab6259d8a6c4a8556b05
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-06 17:05:27 +0000

    file that contain the module player

---
+++ modularity2.py
@@ -0,0 +1,12 @@
+"""
+Module of the player
+
+"""
+def parler(person, message):
+    print("{} : {}".format(person,message))
+
+def good_bye():
+    print("Good bye :) !")
+
+
+# we will import this file in the modularity file
