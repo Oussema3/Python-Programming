205db895877f1af58122e138775d4942b9f9a576
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-20 22:39:43 +0000

    whether ur birthday is important or not

---
+++ birth.py
@@ -0,0 +1,6 @@
+age = int(input("how old are you ? "))
+
+if age < 19 or age  in {21,50} or age > 64:
+    print("your birthday is important :) ")
+else :
+    print("Sorry your birthday is not important :(")
