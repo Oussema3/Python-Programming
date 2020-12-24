9d52bf93b5dcbbcecbf3dc3f4dbe8bc06d568927
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-24 03:48:44 +0000

    function that solve simple equations

---
+++ equation.py
@@ -0,0 +1,8 @@
+def solve_eq(equation):
+    x, add, num1, equal, num2 =equation.split()
+    num1, num2 = int(num1), int(num2)
+    return "x= " + str(num2 - num1)
+
+print(solve_eq("x + 23 = 196"))
+
+
